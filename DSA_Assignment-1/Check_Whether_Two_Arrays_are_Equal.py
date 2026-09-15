90c32d9adb09b16c0d2b1ddd3ec4d7c4922b603a
Shyam-Krishnan24 <r.shyamkrishnan24@gmail.com>
2026-09-15 09:32:00 +0530

    Updated Check whether two arrays are equal method-2

--- DSA_Assignment-1/Check_Whether_Two_Arrays_are_Equal.py
+++ DSA_Assignment-1/Check_Whether_Two_Arrays_are_Equal.py
@@ -8,3 +8,30 @@ def compare(arr1,arr2):
         print("Arrays are not Equal")
 
 compare(arr1,arr2)
+
+# Method -2
+
+if len(arr1)!= len(arr2):
+    print("Arrays are not Equal")
+
+else:
+    equal=True
+
+    for i in range(len(arr1)):
+        count1=0
+        count2=0
+
+        for j in range(len(arr1)):
+            if arr1[i]==arr1[j]:
+                count1+=1
+        for j in range(len(arr2)):
+            if arr1[i]==arr2[j]:
+                count2+=1
+
+        if count1!=count2:
+            equal=False
+            break
+    if equal:
+        print("Arrays are Equal")
+    else:
+        print("Arrays are not Equal")
